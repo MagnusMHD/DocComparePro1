6531c016455f9ea28be8a4f7697a333b72d8644f
Mahdi Mohebi <148750514+MagnusMHD@users.noreply.github.com>
2026-07-14 10:51:46 +0200

    Remove view logic from main window

--- DocComparePro/Views/MainWindow.xaml.cs
+++ DocComparePro/Views/MainWindow.xaml.cs
@@ -1,37 +1,11 @@
-﻿using System.Windows;
-using System.Windows.Input;
+using System.Windows;
 
-namespace DocComparePro.Views
+namespace DocComparePro.Views;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-                DragMove();
-        }
-
-        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState =
-                WindowState == WindowState.Maximized
-                ? WindowState.Normal
-                : WindowState.Maximized;
-        }
-
-        private void BtnClose_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+        InitializeComponent();
     }
 }
